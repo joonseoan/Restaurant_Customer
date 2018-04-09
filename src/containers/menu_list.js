5bcdff77d60f2c64826f62cb36b3cac8f30a6145
Joon An <joonseo.an@outlook.com>
2018-04-08 23:18:12 -0400

    finishes marking the single menu order

--- src/containers/menu_list.js
+++ src/containers/menu_list.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 
@@ -6,69 +6,184 @@ import _ from 'lodash';
 
 import { Link } from 'react-router-dom';
 
-const MenuList =(props) => {
-
-    const path = './images/';
-
-    let firstRow = [];
-    let secondRow = [];
-    let thirdRow = [];
-    let forthRow = [];
-
-    _.map(props.menu, menuType => {
-        
-        firstRow.push(menuType[0]);
-        secondRow.push(menuType[1]);
-        thirdRow.push(menuType[2]);
-        forthRow.push(menuType[3]);
-
-    });
 
 
-        const menuPrices = (item) => {
+let name_price = [];
 
-            return (
+class MenuList extends Component {
 
-               <td key = { item.id } width = "300" height = "50"> 
-                <div>
-                    <input type = "checkbox" value = { item.price }/> 
-                    <label>
-                            
-                            {item.name} (${item.price})
+    constructor(props) {
 
-                    </label>
+        super(props);
 
-                        <p> {item.description} </p>
-                    
-                    <div>
-                        Number of orders:
-                    </div>
+        this.state = {
 
-                    </div>
-               </td>
-                
-            );
+            name_price: [],
+            value : 0
 
         }
 
-        const pictureList = () => {
+    }
 
-            const pictures = (item) => {
-                
-                return (
+    submitValue (event) {
+
+        console.log(event)
+        event.preventDefault();
+        // event.defaultPrevented();
+
+    }
+
+    menuOnChange(event) {
+
+        const name = event.target.name;
+        const value = event.target.value;
+
+        const nameValue = { [name] : value };
+
+        name_price = [...name_price, nameValue];
+
+        this.setState({
+
+            name_price
+            // bgcolor : '#00FF00'
+
+        });
+
+        
+    }
+
+
     
-                    <td key = { item.id } className = "all-menu-pictures">
-                        <Link to = { `/description/${item.name}`} key = { item.id }>
-                                
-                            <img src = { path + item.file } alt = { item.name } width = "200" />
-            
-                        </Link>
-                    </td>
-                
-                );
-            }
+    numberOnChange (event) {
 
+        console.log('event.target',event.target)
+    }
+
+
+    decreaseValue () {
+
+        const value = this.state.value - 1;
+
+        this.setState ({
+
+            value
+
+        });
+    }
+
+    increaseValue () {
+
+        const value = this.state.value + 1;
+
+        this.setState ({
+
+            value
+
+        });
+    }
+    
+
+    allMenuContents () {
+
+        console.log('props: ', this.props)
+
+        const path = './images/';
+
+        let firstRow = [];
+        let secondRow = [];
+        let thirdRow = [];
+        let forthRow = [];
+
+        _.map(this.props.menu, menuType => {
+            
+            firstRow.push(menuType[0]);
+            secondRow.push(menuType[1]);
+            thirdRow.push(menuType[2]);
+            forthRow.push(menuType[3]);
+
+        });
+
+        
+        const menuPrices = (item) => {
+
+            console.log('this.state.pricie', this.state.name_price);
+            console.log('item.name', item.name)
+
+            let bgcolor;
+
+            if (this.state.name_price.length > 0)
+            {
+                console.log('this.state.nameprice[0]:',this.state.name_price[0])
+                var keyNames = Object.keys(this.state.name_price[this.state.name_price.length - 1]);
+                
+                bgcolor = keyNames[0] === item.name ?  '#00FF00' : '';
+
+            }
+           
+
+           //  if (item.name === this.state.name_price)
+
+            return (
+    
+                <td key = { item.name } width = "300" height = "50" bgcolor = { bgcolor }> 
+                
+                    <div >
+    
+                        <div>
+                        
+                            <label>
+                                        
+                                <h5>{ item.name } (${ item.price }) </h5> 
+                                <input type = "checkbox" name = { item.name } 
+                                    value = { item.price } onChange = { this.menuOnChange.bind(this) } />
+                             
+                                
+                                <p> {item.description} </p>
+    
+                            </label>
+    
+                        </div>
+                                
+                        <div>
+    
+                           <label>
+                               Number of orders : 
+                               <div className = "btn btn-primary" onClick = { this.decreaseValue.bind(this) }>-</div>
+                               {this.state.value }
+                               { /*<input type = "number" name = { item.name } 
+                                    value = { this.state.value } onChange = { this.numberOnChange.bind(this) }/> */}
+                               <div className = "btn btn-primary" onClick = { this.increaseValue.bind(this) }>+</div>
+                           </label>
+                                
+                        </div>
+    
+                    </div>
+                
+                </td>
+                    
+            );
+    
+        }
+
+        const pictures = (item) => {
+               
+            return (
+    
+                <td key = { item.name } className = "all-menu-pictures" bgcolor = { this.bgcolorSelect }>
+                    <Link to = { `/description/${item.name}`} key = { item.id }>
+                                
+                       <img src = { path + item.file } alt = { item.name } width = "200" />
+            
+                    </Link>
+                </td>
+                
+            );
+    
+        }
+
+    
         return (
+    
             <tbody className = "all-menu">
 
                 <tr>
@@ -109,7 +224,11 @@ const MenuList =(props) => {
     
     }
 
-    return (
+    render () {
+
+        console.log('this.state.value', this.state.value)
+
+        return (
 
             <div>
 
@@ -117,7 +236,7 @@ const MenuList =(props) => {
                 <div>
                     <legend> <h4> Select foods you would like to order </h4></legend>
 
-                    <form> 
+                    <form onSubmit = { this.submitValue }> 
 
                         <table>
                             <thead>
@@ -137,7 +256,7 @@ const MenuList =(props) => {
                                 </tr>
                             </thead>
                             
-                                { pictureList() }
+                                { this.allMenuContents() }
                             
                         </table>
 
@@ -151,8 +270,19 @@ const MenuList =(props) => {
 
     );
 
+    }
 
-}
+
+} 
+
+ /*
+
+    
+
+
+
+    */
+    
 
 function mapStateToProps ({ menu }) {
 
